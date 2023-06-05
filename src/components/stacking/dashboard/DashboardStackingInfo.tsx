35fb76ee64391767a293b941e7de647168306b90
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-06-05 14:16:21 +0100

    adds stacking dashboard boilerplate

---
+++ src/components/stacking/dashboard/DashboardStackingInfo.tsx
@@ -0,0 +1,89 @@
+import { selectCurrentTheme, selectCurrentUserRole, selectUserSessionState } from '../../../redux/reducers/user-state';
+import { useAppSelector } from '../../../redux/store';
+import { UserRole } from '../../../redux/reducers/user-state';
+import './styles.css';
+import colors from '../../../consts/colorPallete';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+interface DashboardStackingInfoProps {
+  currentRole: UserRole;
+}
+
+const DashboardStackingInfo = ({ currentRole }: DashboardStackingInfoProps) => {
+  const appCurrentTheme = useAppSelector(selectCurrentTheme);
+  return (
+    <div
+      style={{ backgroundColor: colors[appCurrentTheme].infoContainers, color: colors[appCurrentTheme].colorWriting }}
+      className="info-container-dashboard-page"
+    >
+      <div
+        style={{
+          backgroundColor: colors[appCurrentTheme].infoContainers,
+          color: colors[appCurrentTheme].colorWriting,
+          borderBottom: `1px solid ${colors[appCurrentTheme].colorWriting}`,
+        }}
+        className="heading-info-container"
+      >
+        <div className="heading-title-info-container">
+          <div style={{ color: colors[appCurrentTheme].defaultYellow }} className="heading-icon-info-container">
+            <AccountCircleIcon className="icon-info-container yellow-icon" />
+          </div>
+          <div className="title-info-container">INFO</div>
+        </div>
+      </div>
+      <div
+        style={{ backgroundColor: colors[appCurrentTheme].infoContainers, color: colors[appCurrentTheme].colorWriting }}
+        className="content-info-container-normal-user"
+      >
+        <div className="content-sections-title-info-container">
+          <span className="bold-font">Liquidity Provider: </span>
+          <div className="result-of-content-section"></div>
+        </div>
+        <div className="content-sections-title-info-container">
+          <span className="bold-font">List of stackers: </span>
+        </div>
+        <div className="content-sections-title-info-container">
+          <span className="bold-font">Number of Slots Won: </span>
+          <span className="result-of-content-section"></span>
+        </div>
+        <div className="content-sections-title-info-container">
+          <span className="bold-font">Bitcoin Rewards: </span>
+          <span className="result-of-content-section"></span>
+        </div>
+        <div className="content-sections-title-info-container">
+          <span className="bold-font">Total stacked this cycle: </span>
+          <span className="result-of-content-section"></span>
+        </div>
+      </div>
+      {currentRole === 'NormalUser' && (
+        // <div>
+        <div className="footer-join-button-container">
+          <button
+            className={appCurrentTheme === 'light' ? 'customButton' : 'customDarkButton'}
+            onClick={() => {
+              // if (userAddress !== null) {
+              // }
+            }}
+          >
+            Delegate pox-2
+          </button>
+        </div>
+      )}
+      {currentRole === 'NormalUser' && (
+        <div className="footer-join-button-container margin-top-10">
+          <button
+            className={appCurrentTheme === 'light' ? 'customButton' : 'customDarkButton'}
+            onClick={() => {
+              // if (userAddress !== null) {
+              // }
+            }}
+          >
+            Join Pool
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DashboardStackingInfo;
