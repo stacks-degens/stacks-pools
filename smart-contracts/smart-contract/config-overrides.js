7dcd607346adaa04dcdfd41d5d4c6e866d6fdd94
BowTiedDeployer <bowtieddeployer@gmail.com>
2023-08-18 21:11:17 +0300

    add: dependencies for buffer

---
+++ smart-contracts/smart-contract/config-overrides.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+
+// https://github.com/timarney/react-app-rewired
+// https://stackoverflow.com/questions/68707553/uncaught-referenceerror-buffer-is-not-defined
+// https://github.com/facebook/create-react-app/issues/11756
+// https://stackoverflow.com/questions/70398678/i-tried-to-polyfill-modules-in-webpack-5-but-not-working-reactjs
+module.exports = function override(config, env) {
+  // New config, e.g. config.plugins.push...
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer'),
+  };
+  config.resolve.extensions = [...config.resolve.extensions, '.ts', '.js'];
+  config.plugins = [
+    ...config.plugins,
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ];
+
+  return config;
+};
