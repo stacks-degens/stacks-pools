3a4f292cdd24e09091929ed9d3d7fc7590f70f13
BowTiedDeployer <bowtieddeployer@gmail.com>
2023-09-11 03:39:35 +0300

    update: dependencies

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -22,3 +22,28 @@ root.render(
     </BrowserRouter>
   </Provider>
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { store } from '../src/redux/store';
+// import Authenticate from './components/Authenticate';
+
+// import 'stream-browserify';
+// import AppThemeLoader from './components/AppThemeLoader';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <React.StrictMode>
+//           <AppThemeLoader>
+//             <Authenticate />
+//           </AppThemeLoader>
+//         </React.StrictMode>
+//       </BrowserRouter>
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
