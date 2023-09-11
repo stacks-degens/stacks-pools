a74e1c1839a1fc6b512d62287bc6b626bbf0b5a2
BowTiedDeployer <bowtieddeployer@gmail.com>
2023-09-11 04:01:43 +0300

    bug: too many renders react17

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,30 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from '../src/redux/store';
-import Authenticate from './components/Authenticate';
-
-import 'stream-browserify';
-import AppThemeLoader from './components/AppThemeLoader';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <AppThemeLoader>
-          <Authenticate />
-        </AppThemeLoader>
-      </React.StrictMode>
-    </BrowserRouter>
-  </Provider>
-);
-
 // import React from 'react';
-// import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import { BrowserRouter } from 'react-router-dom';
 // import { Provider } from 'react-redux';
@@ -34,16 +9,42 @@ root.render(
 // import 'stream-browserify';
 // import AppThemeLoader from './components/AppThemeLoader';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <BrowserRouter>
-//         <React.StrictMode>
-//           <AppThemeLoader>
-//             <Authenticate />
-//           </AppThemeLoader>
-//         </React.StrictMode>
-//       </BrowserRouter>
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+// root.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <React.StrictMode>
+//         <AppThemeLoader>
+//           <Authenticate />
+//         </AppThemeLoader>
+//       </React.StrictMode>
+//     </BrowserRouter>
+//   </Provider>
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from '../src/redux/store';
+import Authenticate from './components/Authenticate';
+
+import 'stream-browserify';
+import AppThemeLoader from './components/AppThemeLoader';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <React.StrictMode>
+          <AppThemeLoader>
+            <Authenticate />
+          </AppThemeLoader>
+        </React.StrictMode>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
